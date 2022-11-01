e99953d586f15f4ab387a3a9d4f4f4e9bd13e447
Ken <you@example.com>
2022-11-01 11:26:13 +0700

    commit_fullgame

---
+++ Assets/Scripts/Controller/GPlayControoler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GPlayControoler : MonoBehaviour
+{
+
+    public static GPlayControoler instance;
+
+    [SerializeField]
+    private Button instructionButton;
+
+    [SerializeField]
+    private Text scoreText, endscoreText, bestscoreText;
+
+    [SerializeField]
+    private GameObject gameOverPanel, pausePanel;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        _MakeInstance();
+    }
+
+    void _MakeInstance() {
+        if(instance == null) {
+            instance = this;
+        }
+    }
+
+    public void _InstructionButton()
+    {
+        Time.timeScale = 1;
+        instructionButton.gameObject.SetActive(false);
+    }
+
+    public void _setScore(int score) {
+        scoreText.text = "" + score;
+    }
+
+    public void _BirdDiedShowPanel(int score){
+        gameOverPanel.SetActive (true);
+
+        endscoreText.text = "" + score;
+        if(score > GameManager.instance.GetHighScore()) {
+            GameManager.instance.SetHighScore (score);
+        }
+
+        bestscoreText.text = "" + GameManager.instance.GetHighScore();
+    }
+
+    public void _MenuButton() {
+        SceneManager.LoadScene("MainMenu");
+        //Application.LoadLevel("MainMenu");
+    }
+
+    public void _RestartMenuButton(){
+        SceneManager.LoadScene("SampleScene");
+        //Application.LoadLevel("SampleScene");
+        //Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void _PauseButton(){
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void _ResumeButton(){
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Spawner Pipe/SPipe.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPipe : MonoBehaviour
+{
+
+    [SerializeField]
+
+    private GameObject pipeHolder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    // Update is called once per frame
+    IEnumerator Spawner()
+    {
+        yield return new WaitForSeconds(1);
+        Vector3 temp = pipeHolder.transform.position;
+        temp.y = Random.Range(-1.5f, 1.5f);
+
+        Instantiate(pipeHolder, temp, Quaternion.identity);
+        StartCoroutine(Spawner());
+    }
+}
